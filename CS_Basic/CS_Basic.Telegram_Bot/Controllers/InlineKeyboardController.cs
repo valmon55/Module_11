17f31cdf8abb0bbc8507d913cc663d3df1e7395e
valmon55 <fedor_ka@mail.ru>
2023-08-02 15:57:08 +0300

    надо ещё разобраться с флагами

--- CS_Basic/CS_Basic.Telegram_Bot/Controllers/InlineKeyboardController.cs
+++ CS_Basic/CS_Basic.Telegram_Bot/Controllers/InlineKeyboardController.cs
@@ -1,27 +1,45 @@
-﻿using System;
+﻿using CS_Basic.Telegram_Bot.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 
 namespace CS_Basic.Telegram_Bot.Controllers
 {
     internal class InlineKeyboardController
     {
+        private readonly IStorage _memoryStorage;
         private readonly ITelegramBotClient _telegramClient;
 
-        public InlineKeyboardController(ITelegramBotClient telegramBotClient)
+        public InlineKeyboardController(ITelegramBotClient telegramBotClient,IStorage memoryStorage)
         {
             _telegramClient = telegramBotClient;
+            _memoryStorage = memoryStorage;
         }
         public async Task Handle(CallbackQuery? callbackQuery, CancellationToken ct)
         {
-            Console.WriteLine($"Контроллер {GetType().Name} получил сообщение");
+            if (callbackQuery?.Data == null)
+                return;
 
-            await _telegramClient.SendTextMessageAsync(callbackQuery.From.Id, $"Обнаружено нажатие на кнопку {callbackQuery.Data}", cancellationToken: ct);
+            //Console.WriteLine($"Контроллер {GetType().Name} получил сообщение");
+            _memoryStorage.GetSession(callbackQuery.From.Id).LanguageCode = callbackQuery.Data;
+            string languageText = callbackQuery.Data switch
+            {
+                "ru" => " Русский",
+                "en" => " Английский",
+                _ => string.Empty
+            };
+
+            await _telegramClient.SendTextMessageAsync(callbackQuery.From.Id,
+                $"<b>Язык аудио - {languageText}.{Environment.NewLine}</b>" +
+                $"{Environment.NewLine}Можно поменять в главном меню.",
+                cancellationToken: ct,
+                parseMode: ParseMode.Html);
         }
     }
 }
